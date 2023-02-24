e593375a7f1147cb9165f22f836bfaa8a56c08b1
jiashengli <jsli@umd.edu>
2023-02-24 00:24:34 -0500

    Fading finished...

--- Calico_on_bot_code_Ver_2.js
+++ Calico_on_bot_code_Ver_2.js
@@ -1,41 +1,13 @@
-let rbgAuto;
-let rbgFade;
-let notify_interval;
-let rbgAuto_Status = false;
-let rbgFade_Status = false;
-let in_function_led_timeout_1;
-let in_function_led_timeout_2;
-
-function rbgLed(x, y, z) {
-    if (rbgAuto_Status == true) {
-        rbgAuto_Status = false;
-        clearTimeout(in_function_led_timeout_1);
-        clearTimeout(in_function_led_timeout_2);
-        clearInterval(rbgAuto);
-    }
-    if (rbgFade_Status == true) {
-        rbgFade_Status = false;
-        clearInterval(rbgFade);
-    }
-
-    let led_data = [x, y, z, x, y, z, x, y, z];
-    require("neopixel").write(5, led_data);
-}
-
-
-function rbgOff() {
-    if (rbgAuto_Status == true) {
-        rbgAuto_Status = false;
-        clearTimeout(in_function_led_timeout_1);
-        clearTimeout(in_function_led_timeout_2);
-        clearInterval(rbgAuto);
-    }
-    if (rbgFade_Status == true) {
-        rbgFade_Status = false;
-        clearInterval(rbgFade);
-    }
-    require("neopixel").write(5, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
-}
+var notify_interval;
+var fade_rise_interval;
+var fade_down_interval;
+var fade_rise_status = false;
+var fade_down_status = false;
+var rbgAuto;
+var rbgAuto_Status = false;
+var rbgFade_Status = false;
+var in_function_led_timeout_1;
+var in_function_led_timeout_2;
 
 
 function mf() {
@@ -98,7 +70,24 @@ function notification(a) {
 }
 
 
+function rbgLed(x, y, z) {
+    stopPreviousLED();
+    let led_data = [x, y, z, x, y, z, x, y, z];
+    require("neopixel").write(5, led_data);
+}
+
+
+function rbgOff() {
+    stopPreviousLED();
+    require("neopixel").write(5, [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    for (let i = 0; i < 100; i++){
+      console.log('waiting-'+ i);
+    }
+}
+
+
 function rgbAutoChanging() {
+    rbgOff();
     rbgAuto_Status = true;
     rbgAuto = setInterval(function () {
         require("neopixel").write(5, [255, 0, 0, 255, 0, 0, 255, 0, 0]);
@@ -111,16 +100,100 @@ function rgbAutoChanging() {
 }
 
 
+var a = 0;
+var b = 0;
+var c = 0;
+var led_data = [0, 0, 0];
+
+
 function rgbFading(x, y, z) {
+    rbgOff();
     rbgFade_Status = true;
-    let led_data = [x, y, z];
-    rbgFade = setInterval(function (led_data) {
-        for (let i = 0; i < 255; i++){
-
-            led_data = [a, b, c, x, y, z, x, y, z];
-            require("neopixel").write(5, led_data);
-        }
-
-    }, 2000,led_data);
+    led_data = [x, y, z];
+    fade_rise();
 }
 
+
+function fade_rise() {
+    fade_rise_interval = setInterval(function () {
+        fade_rise_status = true;
+        a = a + 5;
+        b = b + 5;
+        c = c + 5;
+        if (a >= led_data[0]) {
+            a = led_data[0];
+        }
+        if (b >= led_data[1]) {
+            b = led_data[1];
+        }
+        if (c >= led_data[2]) {
+            c = led_data[2];
+        }
+        //console.log([a, b, c]);
+        require("neopixel").write(5, [a, b, c, a, b, c, a, b, c]);
+        if (a >= led_data[0] && b >= led_data[1] && c >= led_data[2]) {
+            in_function_led_timeout_1 = setTimeout(function () {
+                fade_down();
+            }, 750);
+            clearInterval(fade_rise_interval);
+            require("neopixel").write(5, [a, b, c, a, b, c, a, b, c]);
+            fade_rise_status = false;
+        }
+    }, 150);
+}
+
+
+function fade_down() {
+    fade_down_interval = setInterval(function () {
+        fade_down_status = true;
+        a = a - 5;
+        b = b - 5;
+        c = c - 5;
+        if (a < 0) {
+            a = 0;
+        }
+        if (b < 0) {
+            b = 0;
+        }
+        if (c < 0) {
+            c = 0;
+        }
+        //console.log([a, b, c]);
+        require("neopixel").write(5, [a, b, c, a, b, c, a, b, c]);
+        if (a <= 0 && b <= 0 && c <= 0) {
+            in_function_led_timeout_2 = setTimeout(function () {
+                fade_rise();
+            }, 750);
+            clearInterval(fade_down_interval);
+            require("neopixel").write(5, [a, b, c, a, b, c, a, b, c]);
+            fade_down_status = false;
+        }
+    }, 150);
+}
+
+function stopFading() {
+    rbgFade_Status = false;
+    if (fade_rise_status == true) {
+        fade_rise_status = false;
+        clearInterval(fade_rise_interval);
+    }
+    if (fade_down_status == true) {
+        fade_down_status = false;
+        clearInterval(fade_down_interval);
+    }
+    clearTimeout(in_function_led_timeout_1);
+    clearTimeout(in_function_led_timeout_2);
+}
+
+
+function stopPreviousLED() {
+    if (rbgAuto_Status == true) {
+        rbgAuto_Status = false;
+        clearTimeout(in_function_led_timeout_1);
+        clearTimeout(in_function_led_timeout_2);
+        clearInterval(rbgAuto);
+    }
+    if (rbgFade_Status == true) {
+        stopFading();
+    }
+}
--- WebFunctions.js
+++ WebFunctions.js
@@ -18,6 +18,13 @@ function rbg() {
 	console.log(blue.value);
 }
 
+function rbgFading() {
+	UART.write(`rgbFading(${green.value/5},${red.value/5},${blue.value/5});\n`);
+	console.log(red.value);
+	console.log(green.value);
+	console.log(blue.value);
+}
+
 function notify(){
 	UART.write(`notification(${timing.value});\n`);
 	console.log(timing.value);
